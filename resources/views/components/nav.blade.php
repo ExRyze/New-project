94a8335bdd9b5499883fcab6249caa7780154a45
ExRyze <vaisyagovinandas@gmail.com>
2022-10-03 21:38:05 +0800

    Create - Navbar

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-primary p-2">
+  <div class="container-fluid d-flex justify-content-between align-items-center text-white">
+    <a href="#" class="text-white d-flex align-items-center text-decoration-none">
+      <img src="src/img/ex_logo.png" alt="ex_logo" width="60" class="me-4">
+      <h2 class="h2">ExBD</h2>
+    </a>
+    <button class="navbar-toggler bg-white color-edge" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a href="#" class="nav-link text-white text-decoration-none">About</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('home');});
+Route::get('/home', function () {return view('home');});
