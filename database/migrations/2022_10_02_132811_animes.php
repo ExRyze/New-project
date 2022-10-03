b3c4639add634bf096404bf541eb0fed94a21a60
ExRyze <vaisyagovinandas@gmail.com>
2022-10-03 20:15:44 +0800

    Create - Anime Table

---
+++ database/migrations/2022_10_02_132811_animes.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Animes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('episodes');
+            $table->string('type')->nullable(); // foreign from table type
+            $table->date('started_airing')->nullable();
+            $table->date('finished_airing')->nullable();
+            $table->enum('status', ['']); // fill the enum
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animes');
+    }
+}
